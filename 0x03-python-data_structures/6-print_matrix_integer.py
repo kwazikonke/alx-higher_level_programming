3d8417fe892855550673eb2856bc75a1dee9f3dc
kwazikonke <21707921@dut4life.ac.za>
2023-08-15 02:04:40 -0700

    Number 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if type(matrix)== list:
+    if type(matrix) == list:
         for i in range(len(matrix)):
-            for x in range(len(matrix[i])):
-                print("{:d}{}".format(matrix[i][j],'' if x == len(matrix[i])-1 else ''), end="")
-                print ("")
+            for j in range(len(matrix[i])):
+                print("{:d}{}".format(matrix[i][j],
+                      '' if j == len(matrix[i]) - 1 else ' '), end="")
+            print("")
