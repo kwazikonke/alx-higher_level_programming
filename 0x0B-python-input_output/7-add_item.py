c7b0d2c0e14aec459fe9f96138da056520d88130
Kwazikonke <21707921@dut4life.ac.za>
2023-09-12 19:24:15 +0200

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    module to add items to a list and save the list
+    as a json string
+"""
+
+import os
+import sys
+
+
+if __name__ == "__main__":
+    load_from_json_file = __import__('6-load_from_json_file')\
+        .load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+    file_is_present = os.path.exists("add_item.json")
+    if not file_is_present:
+        save_to_json_file([], "add_item.json")
+
+    if len(sys.argv) < 2:
+        exit(0)
+
+    my_list = load_from_json_file("add_item.json")
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
+    save_to_json_file(my_list, "add_item.json")
